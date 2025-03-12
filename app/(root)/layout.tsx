f270ab8c4372e6c3a1a58dff680de8422588a84c
Kiet Pham <anhkietpham.hcmus@gmail.com>
2025-03-12 09:36:21 +0700

    setting auth

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "KP Store",
+  description: "KP Ecommerce Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider>
+          {children}
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
