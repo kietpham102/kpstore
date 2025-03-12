1783117e00c14f0511e000854d3eed57bf7be801
Kiet Pham <anhkietpham.hcmus@gmail.com>
2025-03-12 11:13:04 +0700

    feat: usser and wishlist products

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "../globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <ClerkProvider>
           {children}
         </ClerkProvider>
