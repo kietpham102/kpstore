684ddb96d5cad0a777feecd42d53cbbe14541135
Kiet Pham <anhkietpham.hcmus@gmail.com>
2025-03-14 16:29:15 +0700

    update layout products

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
         <ClerkProvider>
+          <Navbar />
           {children}
         </ClerkProvider>
       </body>
